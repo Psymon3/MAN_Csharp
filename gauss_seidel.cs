150bf23460fa3a11134999e2e53809f33dc988de
Psymon3 <lopez.simon68@gmail.com>
2014-05-18 16:54:05 +0200

    fichier gauss seidel

---
+++ gauss_seidel.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MAN_Project
+{
+  public class Gauss_Seidel
+  {
+
+    public static void gauss_seidel(Matrix mat){
+
+      Matrix res = new Matrix(mat.m,mat.n);
+      
+
+      return res;
+
+    }
+
+  }
+    
+}
