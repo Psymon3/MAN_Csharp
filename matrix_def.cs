a7644d2485075287fafa7d5e1bfcf98d03491a84
Guillaume Nominé <gnomine@ibsciss.com>
2014-05-21 14:44:04 +0200

    Suppression algo de base

--- matrix_def.cs
+++ matrix_def.cs
@@ -238,5 +238,28 @@ namespace MAN_Project
                 return res;
             }
         }
+
+
+        /*
+        int determinant(matrix A) { //cette fonction est recursive
+            Matrix T = new Matrix((A.m - 1), (A.m-1));
+            int k = 1;
+            int res;
+            if A.m = 1 {return A.data[0,0];}
+            else {
+                for (int i = 0; i<A.m; i++) { //pour la première colonne
+                    for (int j = 0; j<(A.m -1); j++) {
+                        while (k<(A.m -1)) { // k commence à 1 -> on vire la 1e colonne
+                            if (j < i) { T.data[j,k] = A.data[j,k];}
+                            else if (j > i) { T.data[j-1,k] = A.data[j,k];}
+                        }
+                         
+                    }
+                    res = pow(-1,i+1) * A.data[i,0] * determinant(T); //pow(-1;i+1) = (-1)^(i+1)
+                }
+                return res;
+            }
+        }
+        */
     }
 }
