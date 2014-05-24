2a93e41d8deb93792c1034e167fcd9969c7454a4
Psymon3 <lopez.simon68@gmail.com>
2014-05-24 22:26:56 +0200

    Gauss Seidel marche, mais reste a vérifier

--- gauss_seidel.cs
+++ gauss_seidel.cs
@@ -12,76 +12,44 @@ namespace MAN_Project
       Console.WriteLine("\nWelcome in Gauss Seidel Algorithm");  
       
 
-      double[,] f0 = new double[size,1]; 
-      for(int i=0;i<size;i++)
-      {
-       f0[i,0]= (0);
-      }
 
       Matrix x0 = new Matrix(size,1);
       for(int i=0;i<size;i++)
       {
-       mat.data[i,0]= (0);
+       x0.data[i,0]= (0);
       }
 
       matrixList.Add(x0);
-      Console.WriteLine("1ere matrix");
+      Console.WriteLine("1st matrix");
       matrixList[0].PrintMatrix();
       double diff = 1;
+      double aii = mat.data[0,0];
       for(int k=0;diff>eps && k<ite;k++)
       {
-        double[,] res = new double[size,1];
-        //on rempli res de 0
-        for(int i=0;i<size;i++){
-          res[i,0]=0.0;
-          Console.WriteLine("Remplissage de res en cours : " + res[i,0]);
-        }
-
+        Matrix xkplus1 = new Matrix(size,1);
         for(int i=0;i<size;i++)
         {
-         double o1 = 0;
-         double o2 = 0;
-         for(int j=0;j!=i;j++)
-          {
-            if(!Double.IsNaN(res[j,0])){
-              res[j,0]=0.0;
-            }
-            Console.WriteLine("res["+j+",0] "+res[j,0]);
-            o1 += (mat.data[i,j]*res[j,0]);
-          }
-          Console.WriteLine("o1 "+o1);
-          for(int j=i+1;j<size;j++)
-          {
-            Console.WriteLine("mat.data["+i+","+j+"]="+mat.data[i,j]);
-           /* if(!Double.IsNaN(matrixList[k].data[j, 0])){
-              Console.WriteLine("IsNaN matlist --> 0");
-              matrixList[k].data[j, 0] = 0;
-            }*/
-            Console.WriteLine("matrixList["+k+"].data["+i+",0]="+matrixList[k].data[j, 0]);
-            o2 += (mat.data[i,j]*matrixList[k].data[j, 0]);
-          }
-          Console.WriteLine("o2 "+o2);
-        
-        
-          res[i,0] = (tableau_b[i]-o1-o2);
-          if(!Double.IsNaN(res[i,0]/mat.data[i,i])){
-            res[i,0] = (res[i,0]/mat.data[i,i]);
-          }
-        
+          xkplus1.data[i,0]= (0);
         }
-        Matrix xi = new Matrix(size,1);
-        xi.data = res;
-        Console.WriteLine("Iteration "+k);
-        xi.PrintMatrix();
-        if(Double.IsNaN(xi.data[0,0]))
-        {
-          Console.WriteLine("NaN error --> 0");
-          for(int val = 0; val<size;val++){
-            xi.data[val,0]=0;
+        double o1 = 0;
+        double o2 = 0;
+
+        for(int i = 0 ;i<size;i++){
+          for(int j = 0;j!=i;j++ ){
+            o1 = -(mat.data[i,j]*xkplus1.data[j,0]);
           }
+          for(int j=i+1;j<size;j++){
+            o2 = o2 - (mat.data[i,j]*matrixList[matrixList.Count-1].data[j,0]);
+          }
+          Console.WriteLine("o1 = "+o1+" o2 = "+o2);
+          double val = (tableau_b[i]+o1+o2)/aii;
+          Console.WriteLine("x"+i+"k = "+val);
+          xkplus1.data[i,0]=val;
+            
         }
-        matrixList.Add(xi);
-      
+        Console.WriteLine("iteration k = "+(k+1));
+        xkplus1.PrintMatrix();
+        matrixList.Add(xkplus1);
         if(k>1)
         {
           diff=Math.Abs(matrixList[k-1].data[0, 0] - matrixList[k].data[0, 0]);
@@ -94,15 +62,15 @@ namespace MAN_Project
     }
 
     public static void Test1() {
-      Matrix mat = new Matrix(4,4);
+      Matrix mat = new Matrix(3,3);
       mat.ImportMatrix("gaussseidel.txt");
       mat.PrintMatrix();
-      double[] b = {-19,1,12,1};
+      double[] b = {1,1,1};
       Console.WriteLine("b : ");
       foreach(double d in b){
         Console.Write(d+" ");
       }
-      gauss_seidel(mat, 5, 0.0000000000000001, b);
+      gauss_seidel(mat, 3, 0.0000000000000001, b);
     }    
   }
 }
