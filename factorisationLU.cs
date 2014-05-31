3e05d782a0e4fbf294f6d5c9c4042a998dd74161
Lisa <lisa.peuckert@gmail.com>
2014-05-31 18:20:40 +0200

    LU

--- factorisationLU.cs
+++ factorisationLU.cs
@@ -7,7 +7,7 @@ namespace MAN_Project
 
     public static Matrix[] Factorisation(Matrix A){
 
-	 	Matrix L = new Matrix(A.m, A.n);
+	 	/*Matrix L = new Matrix(A.m, A.n);
 	 	for (int i = 0; i < A.n; i++){
 	 		for (int j = 0; j < A.m; j++){
 	 			if (i == j) { L.data[i, j] = 1.0; }
@@ -32,7 +32,47 @@ namespace MAN_Project
 	 	res[0] = A;
 	 	res[1] = L;
 
+		return res;*/
+		Matrix L = new Matrix(A.m, A.n);
+		Matrix U = new Matrix(A.m, A.n);
+
+		for (int i = 0; i < A.n; i++){
+	 		for (int j = 0; j < A.m; j++){
+	 			if (i == j) { L.data[i, j] = 1.0; }
+	 			else L.data[i, j] = 0.0;
+	 		}
+	 	}
+
+	 	for (int i = 0; i < A.n; i++){
+	 		for (int j = 0; j < A.m; j++){
+	 			U.data[i, j] = A.data[i,j];
+	 		}
+	 	}
+
+	 	for (int i = 0; i < (A.n-1); i++){
+	 		for (int j = i; j<A.n; j++) {
+	 			for (int k = 0; k < i; k++) {
+	 				U.data[i,j] = A.data[i,j] - L.data.[i,k]*U.data[k,j];
+	 			}
+	 		}
+	 		for (int j = i+1; j<A.n;j++) {
+	 			for (int k = 0; k<i; k++) {
+	 				L.data[j,i] = (A.data[j,i]-L.data[j,k]*U.data[k,i])/(U.data[i,i])
+	 			}
+	 		}
+	 	}
+
+	 	for (int k = 0; k <(A.n-1); k++) {
+	 		U.data[A.n;A.n] = A.data[A.n,A.n] - L.data[A.n,k]*U.data[k,A.n];
+	 	}
+
+	 	Matrix[] res = new Matrix[2];
+	 	res[0] = U;
+	 	res[1] = L;
+
 		return res;
+
+
     }
 
     public static void Test1() {
